b23d3c28b5b95c67d2efb61cd0b8fc301c70081e
Kadi K <kadi.kazalov@tktk.ee>
2024-04-23 11:39:11 +0300

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && false --> false (kui üks tingimustest pole täidetud)
+//false && true --> false
+//false && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
